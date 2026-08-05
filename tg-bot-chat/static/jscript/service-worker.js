875a0eded7a89f6ffcafc2daec95c693b6559419
all-about-coding4 <alexndubuisi2003@gmail.com>
2026-08-05 10:21:49 -0700

    worked

---
+++ tg-bot-chat/static/jscript/service-worker.js
@@ -0,0 +1,44 @@
+const CACHE = "telora-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./static/jsons/manifest.json",
+  
+  "./static/style/telora1.css",
+
+  "./static/jscript/helper.js",
+
+  "./static/jscript/user.js",
+  "./static/jscript/watchdog.js",
+  
+  "./static/icons/telora-icon-192.png",
+  "./static/icons/telora-icon-512.png",
+  "./static/icons/telora-icon2-192.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+        .filter(key => key !== CACHE)
+        .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
